2ff20b750deec3ccc70d1d0e576b302cecec1cdd
LenaOsu <lena.osuferraro@live.fr>
2026-08-12 20:05:57 +0200

    Starting a new SQL request to answer to: Do patients with an upper ecog and a bigger weight loss have a darker live pronostic ?

--- app/database/queries/death_ecog_status.sql
+++ app/database/queries/death_ecog_status.sql
@@ -1,7 +1,6 @@
-SELECT p.patient_id, p.ph_ecog, p.wt_loss, f.status, 
+SELECT p.patient_id, p.ph_ecog, p.wt_loss, f.status, f.time_days
 FROM patients p
 JOIN follow_up f ON p.patient_id = f.patient_id
-WHERE p.ph_ecog > 3 AND p.wt_loss > 10
-GROUP BY p.patient_id
-
+WHERE p.ph_ecog >= 3 AND p.wt_loss > 10
+ORDER BY f.status DESC, p.wt_loss DESC, f.time_days ASC
 
