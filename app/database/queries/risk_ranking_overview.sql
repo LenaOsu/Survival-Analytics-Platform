5b5b022ffda8581a3756d2aef6331c8510061f55
LenaOsu <lena.osuferraro@live.fr>
2026-08-18 16:25:13 +0200

    new sql request using cox partial hazard variable finishing

--- app/database/queries/risk_ranking_overview.sql
+++ app/database/queries/risk_ranking_overview.sql
@@ -1,4 +1,4 @@
-SELECT p.patient_id, p.age, p.sex, p.ph_ecog, p.wt_loss, f.status, f.time_days,
+/*SELECT p.patient_id, p.age, p.sex, p.ph_ecog, p.wt_loss, f.status, f.time_days,
 CASE
 WHEN f.status = 1 THEN 'Death'
 WHEN f.status = 0 THEN 'Censored'
@@ -15,4 +15,24 @@ NTILE(4) OVER (ORDER BY p.wt_loss DESC) AS quartile_risque
 FROM patients p
 JOIN follow_up f ON p.patient_id = f.patient_id
 WHERE p.age IS NOT NULL AND p.wt_loss IS NOT NULL
-ORDER BY quartile_risque, rank_weight_loss_time
+ORDER BY quartile_risque, rank_weight_loss_time*/
+
+SELECT p.patient_id, p.age, p.sex, p.ph_ecog, p.wt_loss, f.status, f.time_days, r.partial_hazard,
+CASE
+WHEN f.status = 1 THEN 'Death'
+WHEN f.status = 0 THEN 'Censored'
+END AS status_label,
+CASE
+WHEN p.sex = 1 THEN 'Men'
+WHEN p.sex = 2 THEN 'Women'
+END AS sex_label,
+RANK () OVER (
+    PARTITION BY CASE WHEN p.age < 50 THEN 'less than 50 years' ELSE 'bit older' END
+    ORDER BY r.partial_hazard DESC, f.time_days ASC
+) AS rank_cox_risk,
+NTILE(4) OVER (ORDER BY p.wt_loss DESC) AS quartile_risque
+FROM patients p
+JOIN follow_up f ON p.patient_id = f.patient_id
+JOIN risk_scores_cox r ON p.patient_id = r.patient_id
+WHERE p.age IS NOT NULL AND p.wt_loss IS NOT NULL
+ORDER BY quartile_risque, rank_cox_risk
