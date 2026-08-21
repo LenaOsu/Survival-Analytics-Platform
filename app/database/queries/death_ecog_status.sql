9dffe82b6a89a957adf4659d144be30e5811b465
LenaOsu <lena.osuferraro@live.fr>
2026-08-21 20:52:22 +0200

    cleaning death ecog status sql file

--- app/database/queries/death_ecog_status.sql
+++ app/database/queries/death_ecog_status.sql
@@ -1,10 +1,4 @@
-/*SELECT p.patient_id, p.ph_ecog, p.wt_loss, f.status, f.time_days
-FROM patients p
-JOIN follow_up f ON p.patient_id = f.patient_id
-WHERE p.ph_ecog >= 3 AND p.wt_loss > 10
-ORDER BY f.status DESC, p.wt_loss DESC, f.time_days ASC*/
 
-#########################################
 
 SELECT p.patient_id, p.ph_ecog, f.status,
 CASE 
